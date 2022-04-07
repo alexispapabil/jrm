b8b3158cfb09e8add31087b2236e0907ec666165
alexispapabil <el16019@mail.ntua.gr>
2022-04-07 19:07:59 +0300

    Initial commit

---
+++ job.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import datetime
+from aux import elapsed,tostring
+
+class Job:
+    def __init__(self, id, app, procs, duration, enter, exclusive, start = None, interval = None):
+        self.id = id
+        self.app = app
+        self.procs = procs
+        self.duration = duration
+        self.enter = enter
+        self.exclusive = exclusive
+        self.start = start
+        self.interval = interval
+
+    def startedat(self, timestamp):
+        self.start = timestamp
+
+    def runafter(self, interval):
+        self.interval = interval
+
+    def remaining(self):
+        return round(self.duration-elapsed(self.start))
+
+    def state(self):
+        return 'R' if self.start else 'Q'
+
+    def __str__(self):
+        return str(self.id) + ' ' + ' ' +  self.app + ' ' + ' ' + str(self.procs) + ' ' + ' ' + self.state() + ' ' + ' ' + tostring(self.start) + ' ' + ' ' + str(self.duration)
